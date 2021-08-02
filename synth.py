441e1773a37f39920c5b3738a25c5390f021ceae
marcValentino <mvalentino@id.uff.br>
2021-08-02 18:42:20 -0300

    stuttering and squishification noise solved

--- synth.py
+++ synth.py
@@ -8,7 +8,7 @@ class Synth():
 
     filenames = []
     sps = 44100
-    duration_s = 5
+    duration_s = 0.5
     max_range = 32767
     filename_prefix = "NOTE_"
     filename_extension = ".wav"
@@ -17,17 +17,22 @@ class Synth():
     p = pyaudio.PyAudio()
 
     def synthesizeNote(self, frequency, c1, c2, c3, c4):
-        period = 1/frequency
-        sample_number = np.arange(self.duration_s* self.sps)
-
-        waveform = 0.3 * np.sin((2*np.pi * sample_number/period)/self.sps) * self.max_range
-        waveform2 = 0.3 * np.sin((2*2*np.pi * sample_number/period )/self.sps) * self.max_range
-        waveform3 = 0.3 * np.sin((3*2*np.pi * sample_number/period )/self.sps) * self.max_range
-        waveform4 = 0.3 * np.sin((4*2*np.pi * sample_number/period )/self.sps) * self.max_range
-        final_waveform = c1*waveform + c2*waveform2 + c3*waveform3 + c4*waveform4
-        final_waveform_int16 = np.int16(self.squishify(final_waveform))
+        sample_number = np.arange(int(self.duration_s*frequency)* self.sps)
         wavfile_name = self.filename_prefix + str(frequency) + "-" + str(c1) + "-" + str(c2) + "-" + str(c3) + "-" + str(c4) + self.filename_extension
-        write(wavfile_name, self.sps, final_waveform_int16)
+        try:
+            file_test = wave.open(wavfile_name)
+            file_test.close()
+        except:
+
+            waveform = 0.10 * np.sin((2*np.pi * sample_number*frequency)/self.sps) * self.max_range
+            waveform2 = 0.10 * np.sin((2*2*np.pi * sample_number*frequency)/self.sps) * self.max_range
+            waveform3 = 0.10 * np.sin((3*2*np.pi * sample_number*frequency)/self.sps) * self.max_range
+            waveform4 = 0.10 * np.sin((4*2*np.pi * sample_number*frequency)/self.sps) * self.max_range
+            base_waveform = c1*waveform + c2*waveform2 + c3*waveform3 + c4*waveform4
+            base_waveform_int16 = np.int16(self.squishify(base_waveform))
+            
+            write(wavfile_name, self.sps, base_waveform_int16)
+
         self.filenames.append(wavfile_name)
         return wavfile_name
 
@@ -51,28 +56,36 @@ class Synth():
         note_files = self.get_file_pointers()
 
         while True:
-            final_waveform = base_waveform
+            if len(note_files) == 0: return
             for file in note_files:
                 file_buffer = file.readframes(self.chunk_size)
                 if len(file_buffer) <= 0:
                     file.rewind()
+                    file_buffer = file.readframes(2)
                     file_buffer = file.readframes(self.chunk_size)
                 float32_array_from_buffer = np.frombuffer(file_buffer, dtype=np.int16).astype(np.float32)
-                if len(float32_array_from_buffer) < self.chunk_size:
+                if len(float32_array_from_buffer) < self.chunk_size: 
                     file.rewind()
                     file_buffer = file.readframes(self.chunk_size - len(float32_array_from_buffer))
                     buffer_complement = np.frombuffer(file_buffer, dtype=np.int16).astype(np.float32)
                     print("COMPLEMENT")
-                    
-                    float32_array_from_buffer = np.concatenate([float32_array_from_buffer, buffer_complement])
+                    print(base_waveform)
+                    slice_index = np.argmax(buffer_complement < float32_array_from_buffer[len(float32_array_from_buffer)-1])
+                    float32_array_from_buffer = np.concatenate([float32_array_from_buffer, buffer_complement[slice_index:]])
+                    if len(float32_array_from_buffer) < self.chunk_size:
+                        
+                        file_buffer = file.readframes(self.chunk_size - len(float32_array_from_buffer))
+                        buffer_complement = np.frombuffer(file_buffer, dtype=np.int16).astype(np.float32)
+                        float32_array_from_buffer = np.concatenate([float32_array_from_buffer, buffer_complement])
                     # file.rewind()
-                final_waveform = final_waveform + float32_array_from_buffer
+                base_waveform = base_waveform + float32_array_from_buffer
+            # if base_waveform:
+            base_waveform = np.int16(self.squishify(base_waveform))
+            print("FINAL CHUNK: {}".format(base_waveform))
+            base_waveform = base_waveform.tobytes()
             
-            final_waveform = np.int16(self.squishify(final_waveform))
-            final_waveform = final_waveform.tobytes()
-
-            stream.write(final_waveform)
-
+            stream.write(base_waveform)
+            base_waveform = np.zeros(self.chunk_size, np.float32)
 
     def get_file_pointers(self):
         pointers = []
@@ -87,9 +100,10 @@ if __name__ == '__main__':
 
     synth = Synth()
     ratio = 1.05946
-    base_frequency = 125
-    filename_1st = synth.synthesizeNote(base_frequency, 1, 0.5, 0.25, 0.125)
-    filename_3rd = synth.synthesizeNote(base_frequency * (ratio **4), 1, 0.5, 0.25, 0.125)
-    filename_5th = synth.synthesizeNote(base_frequency * (ratio **7), 1, 0.5, 0.25, 0.125)
+    base_frequency = 200
+    filename_1st = synth.synthesizeNote(base_frequency, 1, 0.7, 0.8, 0.3)
+    filename_3rd = synth.synthesizeNote(base_frequency * (ratio **4), 1, 0.7, 0.8, 0.3)
+    filename_5th = synth.synthesizeNote(base_frequency * (ratio **7), 1, 0.7, 0.8, 0.3)
+    filename_7th = synth.synthesizeNote(base_frequency * (ratio **10), 1, 0.7, 0.8, 0.3)
     synth.play()
     
